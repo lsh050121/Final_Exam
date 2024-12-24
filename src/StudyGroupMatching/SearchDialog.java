32cfc29355acd505bf48bbf96c109d23c5cdd173
SangHyeok Lee <lsh050121@naver.com>
2024-12-25 01:29:57 +0900

    SearchDialog 클래스 Javadoc 작성

--- src/StudyGroupMatching/SearchDialog.java
+++ src/StudyGroupMatching/SearchDialog.java
@@ -16,11 +16,14 @@ import java.util.stream.Collectors;
  * @changelog
  * <ul>
  *     <li>2024-12-24 : 최초생성</li>
+ *     <li>2024-12-24 : 검색 버튼 검사 기능 추가</li>
  * </ul>
  */
 public class SearchDialog extends JFrame{
     /**
-     * 검색창을 생성하는 메소드입니다.
+     * 요일별 스터디 그룹을 검색하는 검색창을 여는 메소드입니다.
+     * 사용자가 특정 요일을 선택하면, 해당 요일에 해당하는 스터디 그룹을 필터링 하여 보여줍니다.
+     * 검색 결과가 없으면 해당 요일에 스터디가 없다라느 메세지를 띄워줍니다.
      *
      * @authro Lee SangHyeok    (lsh050121@naver.com)
      *
@@ -30,8 +33,8 @@ public class SearchDialog extends JFrame{
      * <ul>
      *     <li>2024-12-24 : 최초생성</li>
      * </ul>
-     * @param parent JFrame 객체인 호출한 창, 부모창입니다.
-     * @param studyListPanel 결과값을 mainFrame의 studyListPanel에 반환합니다
+     * @param parent 부모 JFrame 객체, 다이얼로그는 이 부모 JFrame 위에 표시됩니다.
+     * @param studyListPanel 스터디 그룹 리스트를 표시하는 JPanel 객체, 검색 후 필터링된 스터디 그룹 목록을 이 패널에 갱신합니다.
      */
     public static void openSearchDialog(JFrame parent, JPanel studyListPanel){
         JDialog searchDialog = new JDialog(parent, "요일별 스터디 검색", true);
@@ -82,9 +85,9 @@ public class SearchDialog extends JFrame{
     /**
      * 주어진 요일에 해당하는 스터디 그룹들을 필터링하여 반환하는 메소드입니다.
      *
-     * <p>이 메소드는 "src/StudyGroupMatching/StudyGroup_List.txt" 파일을 읽어
+     * 이 메소드는 "src/StudyGroupMatching/StudyGroup_List.txt" 파일을 읽어
      * 각 스터디 그룹 정보에서 주어진 요일이 포함된 그룹만 필터링하여 리스트로 반환합니다.
-     * 만약 파일을 읽는 중 오류가 발생하면 오류 메시지를 포함한 리스트를 반환합니다.</p>
+     * 만약 파일을 읽는 중 오류가 발생하면 오류 메시지를 포함한 리스트를 반환합니다.
      *
      * @param day 필터링할 요일. 예를 들어, "월요일", "화요일" 등 요일 문자열을 입력합니다.
      * @return 주어진 요일에 해당하는 스터디 그룹들의 리스트.
